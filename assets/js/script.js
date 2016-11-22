89361dd475f1f7b63ae654991f35f1b1af8d85e2
Hilen <hilenkz@gmail.com>
2016-11-22 22:33:14 +0800

    Site updated: 2016-11-22 22:33:14

--- assets/js/script.js
+++ assets/js/script.js
@@ -11901,30 +11901,58 @@ return jQuery;
   // Run fancybox feature
 
   $(document).ready(function() {
-    $(".fancybox").fancybox({
-      maxWidth: 900,
-      maxHeight: 800,
-      fitToView: true,
-      width: '50%',
-      height: '50%',
-      autoSize: true,
-      closeClick: false,
-      openEffect: 'elastic',
-      closeEffect: 'elastic',
-      prevEffect: 'none',
-      nextEffect: 'none',
-      padding: '0',
-      helpers: {
-        thumbs: {
+    /**
+     * Configure and run Fancybox plugin
+     * @returns {void}
+     */
+    function fancyFox() {
+      var arrows = true;
+      var thumbs = null;
+
+      // disable navigation arrows and display thumbs on medium and large screens
+      if ($(window).height() > 480) {
+        arrows = false;
+        thumbs = {
           width: 70,
           height: 70
-        },
-        overlay: {
-          css: {
-            background: 'rgba(0, 0, 0, 0.85)'
-          }
-        }
+        };
       }
+
+      $('.fancybox').fancybox({
+        maxWidth: 900,
+        maxHeight: 800,
+        fitToView: true,
+        width: '50%',
+        height: '50%',
+        autoSize: true,
+        arrows: arrows,
+        closeClick: false,
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        prevEffect: 'none',
+        nextEffect: 'none',
+        padding: '0',
+        helpers: {
+          thumbs: thumbs,
+          overlay: {
+            css: {
+              overflow: 'hidden',
+              background: 'rgba(0, 0, 0, 0.85)'
+            }
+          }
+        },
+        afterLoad: function() {
+          setTimeout(function() {
+            $('.fancybox-next > span, .fancybox-prev > span').css('visibility', 'visible');
+          }, 400);
+        }
+      });
+    }
+
+    fancyFox();
+    
+    $(window).smartresize(function() {
+      fancyFox();
     });
   });
 })(jQuery);
@@ -12070,7 +12098,7 @@ return jQuery;
 
         // Checks if image width is smaller than his box
         if (imageWidth < photoBoxWidth) {
-          imageRatio = (imageHeight / photoBoxWidth);
+          imageRatio = (imageHeight / imageWidth);
 
           $image.css({
             width: photoBoxWidth,
@@ -12172,6 +12200,219 @@ return jQuery;
     if ($('.post-bottom-bar').length) {
       var postBottomBar = new PostBottomBar();
       postBottomBar.run();
+    }
+  });
+})(jQuery);
+;(function($) {
+  'use strict';
+
+  /**
+   * Search modal with Algolia
+   * @constructor
+   */
+  var SearchModal = function() {
+    this.$openButton = $('.open-algolia-search');
+    this.$searchModal = $('#algolia-search-modal');
+    this.$closeButton = this.$searchModal.find('.close-button');
+    this.$searchForm = $('#algolia-search-form');
+    this.$searchInput = $('#algolia-search-input');
+    this.$results = this.$searchModal.find('.results');
+    this.$noResults = this.$searchModal.find('.no-result');
+    this.$resultsCount = this.$searchModal.find('.results-count');
+    this.algolia = algoliaIndex;
+  };
+
+  SearchModal.prototype = {
+    /**
+     * Run feature
+     * @returns {void}
+     */
+    run: function() {
+      var self = this;
+
+      // open modal when open button is clicked
+      self.$openButton.click(function() {
+        self.open();
+      });
+
+      // open modal when `s` button is pressed
+      $(document).keyup(function(event) {
+        var target = event.target || event.srcElement;
+        // exit if user is focusing an input
+        if (target.tagName.toUpperCase() === 'INPUT') {
+          return;
+        }
+
+        if (event.keyCode === 83 && !self.$searchModal.is(':visible')) {
+          self.open();
+        }
+      });
+
+      // close button when overlay is clicked
+      self.$searchModal.click(function(e) {
+        if (e.target === this) {
+          self.close();
+        }
+      });
+
+      // close modal when close button is clicked
+      self.$closeButton.click(function() {
+        self.close();
+      });
+
+      // close modal when `ESC` button is pressed
+      $(document).keyup(function(e) {
+        if (e.keyCode === 27 && self.$searchModal.is(':visible')) {
+          self.close();
+        }
+      });
+
+      // send search when form is submitted
+      self.$searchForm.submit(function(event) {
+        event.preventDefault();
+        self.search(self.$searchInput.val());
+      });
+    },
+
+    /**
+     * Open search modal and display overlay
+     * @returns {void}
+     */
+    open: function() {
+      this.showSearchModal();
+      this.showOverlay();
+      this.$searchInput.focus();
+    },
+
+    /**
+     * Close search modal and overlay
+     * @returns {void}
+     */
+    close: function() {
+      this.hideSearchModal();
+      this.hideOverlay();
+      this.$searchInput.blur();
+    },
+
+    /**
+     * Search with Algolia API and display results
+     * @param {String} search
+     * @returns {void}
+     */
+    search: function(search) {
+      var self = this;
+      this.algolia.search(search, function(err, content) {
+        if (!err) {
+          self.showResults(content.hits);
+          self.showResultsCount(content.nbHits);
+        }
+      });
+    },
+
+    /**
+     * Display results
+     * @param {Array} posts
+     * @returns {void}
+     */
+    showResults: function(posts) {
+      var html = '';
+      posts.forEach(function(post) {
+        var lang = window.navigator.userLanguage || window.navigator.language || post.lang;
+
+        html += '<div class="media">';
+        if (post.thumbnailImageUrl) {
+          html += '<div class="media-left">';
+          html += '<a class="link-unstyled" href="' + (post.link || post.permalink) +'">';
+          html += '<img class="media-image" ' +
+            'src="' + post.thumbnailImageUrl + '" ' +
+            'width="90" height="90"/>';
+          html += '</a>';
+          html += '</div>';
+        }
+
+        html += '<div class="media-body">';
+        html += '<a class="link-unstyled" href="' + (post.link || post.permalink) +'">';
+        html += '<h3 class="media-heading">' + post.title + '</h3>';
+        html += '</a>';
+        html += '<span class="media-meta">';
+        html += '<span class="media-date text-small">';
+        html += moment(post.date).locale(lang).format('ll');
+        html += '</span>';
+        html += '</span>';
+        html += '<div class="media-content hide-xs font-merryweather">' + post.excerpt + '</div>';
+        html += '</div>';
+        html += '<div style="clear:both;"></div>';
+        html += '<hr>';
+        html += '</div>';
+      });
+      this.$results.html(html);
+    },
+
+    /**
+     * Show search modal
+     * @returns {void}
+     */
+    showSearchModal: function() {
+      this.$searchModal.fadeIn();
+    },
+
+    /**
+     * Hide search modal
+     * @returns {void}
+     */
+    hideSearchModal: function() {
+      this.$searchModal.fadeOut();
+    },
+
+    /**
+     * Display messages and counts of results
+     * @param {Number} count
+     * @returns {void}
+     */
+    showResultsCount: function(count) {
+      var string = '';
+      if (count < 1) {
+        string = this.$resultsCount.data('message-zero');
+        this.$noResults.show();
+      }
+      else if (count === 1) {
+        string = this.$resultsCount.data('message-one');
+        this.$noResults.hide();
+      }
+      else if (count > 1) {
+        string = this.$resultsCount.data('message-other').replace(/\{n\}/, count);
+        this.$noResults.hide();
+      }
+      this.$resultsCount.html(string);
+    },
+
+    /**
+     * Show overlay
+     * @returns {void}
+     */
+    showOverlay: function() {
+      $('body').append('<div class="overlay"></div>');
+      $('.overlay').fadeIn();
+      $('body').css('overflow', 'hidden');
+    },
+
+    /**
+     * Hide overlay
+     * @returns {void}
+     */
+    hideOverlay: function() {
+      $('.overlay').fadeOut(function() {
+        $(this).remove();
+        $('body').css('overflow', 'auto');
+      });
+    }
+  };
+
+  $(document).ready(function() {
+    // launch feature only if there is an Algolia index available
+    if (typeof algoliaIndex !== 'undefined') {
+      var searchModal = new SearchModal();
+      searchModal.run();
     }
   });
 })(jQuery);
@@ -12285,6 +12526,7 @@ return jQuery;
     // If you change value of `mediumScreenWidth`,
     // you have to change value of `$screen-min: (md-min)` too
     // in `source/_css/utils/variables.scss`
+    this.$body = $('body');
     this.mediumScreenWidth = 768;
   };
 
@@ -12296,13 +12538,13 @@ return jQuery;
     run: function() {
       var self = this;
       // Detect the click on the open button
-      self.$openBtn.click(function() {
+      this.$openBtn.click(function() {
         if (!self.$sidebar.hasClass('pushed')) {
           self.openSidebar();
         }
       });
       // Detect the click on close button
-      self.$closeBtn.click(function() {
+      this.$closeBtn.click(function() {
         if (self.$sidebar.hasClass('pushed')) {
           self.closeSidebar();
         }
@@ -12362,10 +12604,11 @@ return jQuery;
       var self = this;
       // Check if the sidebar isn't swiped
       // and prevent multiple click on the open button with `.processing` class
-      if (!self.$sidebar.hasClass('pushed') && !this.$sidebar.hasClass('processing')) {
+      if (!this.$sidebar.hasClass('pushed') && !this.$sidebar.hasClass('processing')) {
         // Swipe the sidebar to the right
-        self.$sidebar.addClass('processing pushed');
-
+        this.$sidebar.addClass('processing pushed');
+        // add overflow on body to remove horizontal scroll
+        this.$body.css('overflow-x', 'hidden');
         setTimeout(function() {
           self.$sidebar.removeClass('processing');
         }, 250);
@@ -12377,14 +12620,13 @@ return jQuery;
      * @return {void}
      */
     swipeSidebarToLeft: function() {
-      var self = this;
       // Check if the sidebar is swiped
       // and prevent multiple click on the close button with `.processing` class
-      if (self.$sidebar.hasClass('pushed') && !this.$sidebar.hasClass('processing')) {
+      if (this.$sidebar.hasClass('pushed') && !this.$sidebar.hasClass('processing')) {
         // Swipe the sidebar to the left
-        self.$sidebar
-          .addClass('processing')
-          .removeClass('pushed processing');
+        this.$sidebar.addClass('processing').removeClass('pushed processing');
+        // go back to the default overflow
+        this.$body.css('overflow-x', 'auto');
       }
     },
 
@@ -12396,9 +12638,9 @@ return jQuery;
       var self = this;
       // Check if the blog isn't swiped
       // and prevent multiple click on the open button with `.processing` class
-      if (!self.$blog.hasClass('pushed') && !this.$blog.hasClass('processing')) {
+      if (!this.$blog.hasClass('pushed') && !this.$blog.hasClass('processing')) {
         // Swipe the blog to the right
-        self.$blog.addClass('processing pushed');
+        this.$blog.addClass('processing pushed');
 
         setTimeout(function() {
           self.$blog.removeClass('processing');
@@ -12416,9 +12658,7 @@ return jQuery;
       // and prevent multiple click on the close button with `.processing` class
       if (self.$blog.hasClass('pushed') && !this.$blog.hasClass('processing')) {
         // Swipe the blog to the left
-        self.$blog
-          .addClass('processing')
-          .removeClass('pushed');
+        self.$blog.addClass('processing').removeClass('pushed');
 
         setTimeout(function() {
           self.$blog.removeClass('processing');
